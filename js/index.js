17404ebc8384dc7205c9e12b97be5931df75291a
lowkey <saltyndea@gmail.com>
2021-10-17 14:56:42 -0600

    Simp[lified menu animation. Revisit as stretch goal

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 const navicon = document.querySelector('.navicon');
 
-
+let naviconOpen = false;
 
 
 const toggleNavBtn = () => {
-  navicon.classList.toggle('active');
+  if(!naviconOpen) {
+    navicon.classList.add('active');
+    naviconOpen = true;
+  } else {
+    navicon.classList.remove('active');
+    naviconOpen = false;
+  }
 }
 
 
