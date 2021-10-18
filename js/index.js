66a3d0452362a7912aadce163706bee3fdd9eafb
lowkey <saltyndea@gmail.com>
2021-10-17 21:11:43 -0600

    First pass of overlay

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 const navicon = document.querySelector(".navicon");
+const menu = document.querySelector(".menu-overlay");
+
+const toggleOverlay = () => {
+  menu.classList.toggle("open");
+};
 
 const toggleNavBtn = () => {
   navicon.classList.toggle("active");
+
+  toggleOverlay();
 };
 
 navicon.addEventListener("click", toggleNavBtn);
